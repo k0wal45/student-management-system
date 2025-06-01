c8c5bd8d65f20d5eae20ebcd8594bc6639af5b8f
KrzysztofKantorski <krzysiu8845@wp.pl>
2025-06-01 16:05:15 +0200

    Połączenie mongodb

--- student-management-system/student-management-system.cpp
+++ student-management-system/student-management-system.cpp
@@ -1,12 +1,19 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
+
 #include <iostream>
 #include <cstdlib>
+#include <bsoncxx/json.hpp>
+#include <mongocxx/client.hpp>
+#include <mongocxx/instance.hpp>
+#include <mongocxx/uri.hpp>
+#include <mongocxx/database.hpp>
+#include <mongocxx/collection.hpp>
 
 using namespace std;
 
 int main() {
+    // Sprawdzenie zmiennej środowiskowej WORD
     const char* word = getenv("WORD");
-
     if (word) {
         cout << "The value of WORD is: " << word << endl;
     }
@@ -14,7 +21,21 @@ int main() {
         cout << "Environment variable WORD is not set." << endl;
     }
 
-    cout << "pls work this time too" << endl;
+    // Inicjalizacja drivera MongoDB
+    mongocxx::instance instance{};
+    mongocxx::client client{ mongocxx::uri{} };
+
+    // Połączenie z bazą i kolekcją
+    mongocxx::database db = client["test"];              // baza 'test'
+    mongocxx::collection coll = db["example"];           // kolekcja 'example'
+
+    // Wstawienie dokumentu
+    bsoncxx::builder::stream::document document{};
+    document << "word" << (word ? word : "undefined");
+
+    coll.insert_one(document.view());
+
+    cout << "Inserted a document into MongoDB." << endl;
 
     return 0;
 }
