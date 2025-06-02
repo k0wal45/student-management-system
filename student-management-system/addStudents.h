6fd7e0ccea19533296367253dd2928ce406b0f33
k0wal45 <d.kowal.com@gmail.com>
2025-06-02 20:05:52 +0200

    database

---
+++ student-management-system/addStudents.h
@@ -0,0 +1,10 @@
+#ifndef ADDSTUDENTS_H
+#define ADDSTUDENTS_H
+
+#include <string>
+
+void createStudent();
+void addStudentToFile(const std::string& filePath);
+void printStudents(const std::string& filePath);
+
+#endif
--- student-management-system/student-management-system.cpp
+++ student-management-system/student-management-system.cpp
@@ -1,41 +1,32 @@
-﻿#define _CRT_SECURE_NO_WARNINGS
+﻿#include <iostream>
 
-#include <iostream>
-#include <cstdlib>
-#include <bsoncxx/json.hpp>
-#include <mongocxx/client.hpp>
-#include <mongocxx/instance.hpp>
-#include <mongocxx/uri.hpp>
-#include <mongocxx/database.hpp>
-#include <mongocxx/collection.hpp>
+#include "addStudents.h"
+#include "json.hpp"
 
 using namespace std;
 
 int main() {
-    // Sprawdzenie zmiennej środowiskowej WORD
-    const char* word = getenv("WORD");
-    if (word) {
-        cout << "The value of WORD is: " << word << endl;
+
+
+    string path = "students.json"; // Upewnij się, że ścieżka jest poprawna
+
+    cout << "System zarzadzania studentami\n";
+    cout << "1. Pokaz studentow\n";
+    cout << "2. Wyjdz\n";
+    cout << "Wybierz opcje: ";
+    int option;
+    cin >> option;
+    cin.ignore();
+
+    if (option == 1) {
+        printStudents(path);
+    }
+    else if (option == 2) {
+        cout << "Wyjscie z programu.\n";
     }
     else {
-        cout << "Environment variable WORD is not set." << endl;
+        cout << "Nieprawidlowa opcja.\n";
     }
-
-    // Inicjalizacja drivera MongoDB
-    mongocxx::instance instance{};
-    mongocxx::client client{ mongocxx::uri{} };
-
-    // Połączenie z bazą i kolekcją
-    mongocxx::database db = client["test"];              // baza 'test'
-    mongocxx::collection coll = db["example"];           // kolekcja 'example'
-
-    // Wstawienie dokumentu
-    bsoncxx::builder::stream::document document{};
-    document << "word" << (word ? word : "undefined");
-
-    coll.insert_one(document.view());
-
-    cout << "Inserted a document into MongoDB." << endl;
 
     return 0;
 }
